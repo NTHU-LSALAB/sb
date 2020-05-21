d005e9538c219804bc26ef89ddd99fcbc80bab8f
afg <afg984@gmail.com>
2020-05-21 13:58:13 +0800

    include bold font

--- cmd/sb/embed.go
+++ cmd/sb/embed.go
@@ -16,7 +16,7 @@ const htmlTemplateString = `<!DOCTYPE html>
   </script>
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
   <style>
     .best {
       font-weight: bold;
